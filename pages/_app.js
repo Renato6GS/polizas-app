d0a63287d7ba7300511d478c868821d6c1b43d15
Renatogranados <renatogranadosgt@gmail.com>
2022-05-15 23:03:34 -0600

    feat: add loader component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import { I18nProvider } from 'context/i18n';
 import '../styles/globals.css';
+import Loader from 'components/Loader';
+import Router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+
+  Router.events.on('routeChangeStart', () => setLoading(true));
+  Router.events.on('routeChangeComplete', () => setLoading(false));
+
   return (
     <I18nProvider>
+      {loading && <Loader />}
       <Component {...pageProps} />
     </I18nProvider>
   );
